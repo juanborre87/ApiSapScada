d50226768de079dea42b37b28ec102688181c635
Juan Borre <jab02mc@gmail.com>
2025-07-01 00:31:30 -0500

    Update

--- src/Domain/Dtos/ProductDescriptionDto.cs
+++ src/Domain/Dtos/ProductDescriptionDto.cs
@@ -4,6 +4,13 @@ namespace Domain.Dtos
 {
     public class ProductDescriptionDto
     {
+        [JsonProperty("results")]
+        public List<ProductDescriptionResultDto> Results { get; set; }
+
+    }
+
+    public class ProductDescriptionResultDto
+    {
         [JsonProperty("Product")]
         public string Product { get; set; }
 
